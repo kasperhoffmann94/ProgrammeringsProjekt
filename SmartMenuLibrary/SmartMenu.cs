da5a058a930a8dcf0484758bfd7e702d19eaae2c
LAPTOP-4LGA03E1\kaspe <kasperhoffmann94@gmail.com>
2018-09-26 08:45:58 +0200

    Add project files.

---
+++ SmartMenuLibrary/SmartMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMenuLibrary
+{
+    public class SmartMenu
+    {
+        public void LoadMenu(string path)
+        {
+            // Implement ...
+            string s;
+
+        }
+        public void Activate()
+        {
+            // Implement ...
+        }
+    }
+}
