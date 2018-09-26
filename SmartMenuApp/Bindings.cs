fedc2406643aa3c34c8a4f3ad62c32a890575621
LAPTOP-4LGA03E1\kaspe <kasperhoffmann94@gmail.com>
2018-09-26 11:59:16 +0200

    added binding class to app

---
+++ SmartMenuApp/Bindings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMenuApp
+{
+    class Bindings
+    {
+
+        public string Call(string id)
+        {
+
+            return id;
+        }
+    }
+}
