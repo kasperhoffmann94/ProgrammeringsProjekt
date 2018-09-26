78566a3dbf62984fce4b8c2b7ed0f89d1ec1d93a
LAPTOP-4LGA03E1\kaspe <kasperhoffmann94@gmail.com>
2018-09-26 10:41:38 +0200

    Splittet string array, into multiple strings to handle by activator

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -8,31 +8,47 @@ namespace SmartMenuLibrary
 {
     public class SmartMenu
     {
+
+        string line;
+        string[] lines = new string[5];
+        string titel = "";
+        string explain = "";
+        string firstMenuPoint = "";
+        string secondMenuPoint = "";
+        string thirdMenuPoint = "";
         public void LoadMenu(string path)
         {
             int counter = 0;
-            string line;
+          
 
             // Read the file and display it line by line.  
             System.IO.StreamReader file =
                 new System.IO.StreamReader(@"..\..\..\MenuSpec.txt");
             while ((line = file.ReadLine()) != null)
             {
-                System.Console.WriteLine(line);
+                lines[counter] = line;
                 counter++;
             }
 
+            foreach (string temp in lines[2].Split(';'))
+            {
+                titel = lines[0];
+                explain = lines[1];
+                firstMenuPoint = "1. " + lines[2] + ": ";
+                secondMenuPoint = "2. " + lines[3] + ": ";
+                thirdMenuPoint = "3. " + lines[4] + ": ";
+                counter++;
+            }
+
+
             file.Close();
-            System.Console.WriteLine("There were {0} lines.", counter);
-            // Suspend the screen.  
-            System.Console.ReadLine();
+  
 
         }
         public void Activate()
         {
+            Console.WriteLine(titel);
 
-            Console.WriteLine("titel");
-            // Implement ...
         }
     }
 }
