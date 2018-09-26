836f7259ca6eb6587ce8d719df16b2299d095b98
LAPTOP-4LGA03E1\kaspe <kasperhoffmann94@gmail.com>
2018-09-26 09:26:30 +0200

    added the read a file line for line

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -10,8 +10,22 @@ namespace SmartMenuLibrary
     {
         public void LoadMenu(string path)
         {
-            // Implement ...
-         
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"..\..\MenuSpec.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            System.Console.ReadLine();
 
         }
         public void Activate()
