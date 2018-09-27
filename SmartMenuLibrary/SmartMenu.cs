23c36289a3ba59fd37a5df8d379343d28f9350e1
LAPTOP-4LGA03E1\kaspe <kasperhoffmann94@gmail.com>
2018-09-27 07:34:34 +0200

    Added the smart menu classes and added behavior to print out the menu

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace SmartMenuLibrary
 {
     public class SmartMenu
@@ -13,12 +12,12 @@ namespace SmartMenuLibrary
         string[] lines = new string[5];
         string titel = "";
         string explain = "";
-        string menuPoint = "";
-        bool exit = false; 
+        string[,] menu;
+        bool exit = false;
         public void LoadMenu(string path)
         {
             int counter = 0;
-          
+
 
             // Read the file and display it line by line.  
             System.IO.StreamReader file =
@@ -34,60 +33,80 @@ namespace SmartMenuLibrary
             explain = lines[1];
             int count = 1;
 
+
+            //indlæs menupunkter
+            menu = new string[lines.Length - 2, 2];
             for (int i = 2; i < lines.Length; i++)
             {
-                tempString = lines[i].Split(';');
-                menuPoint =  "1. " + tempString[0] + ": ";
-                menuPoint += "\n" + "2. " + tempString[2] + ": "; 
+                string[] split = lines[i].Split(';');
+                // menu title
+                menu[i - 2, 0] = split[0];
+                // menu ID
+                menu[i - 2, 1] = split[1];
 
-              
-                
             }
-                
-                counter++;
-            
 
 
             file.Close();
-  
+
 
         }
         public void Activate()
         {
-          
-            Console.WriteLine(titel + "\n");
 
-            Console.WriteLine(explain + "\n");
-
-            Console.WriteLine(menuPoint + "\n");
-
-            int point = int.Parse(Console.ReadLine());
-            
-            switch (point)
+            int choice = 0;
+            while (true)
             {
-                case 0:
-                    exit = true;
+
+                Console.Clear();
+
+                Console.WriteLine(titel + "\n");
+
+                Console.WriteLine(explain + "\n");
+
+                // print alle menuerne
+                for (int i = 0; i < menu.GetLength(0); i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + menu[i, 0]);
+                }
+
+                string input = Console.ReadLine();
+
+
+                if (input == "0")
+                {
                     break;
+                }
+                else if (int.TryParse(input, out choice))
+                {
+                    choice -= 1;
+
+                    //kald bindingsklassen
+                    if (choice < menu.GetLength(0))
+                    {
+
+                    } else
+                    {
+                        Console.WriteLine("The number you put in is too high, choose one from the description");
+
+                    }
+                }
+
+                else if (input.Length <= 0)
+                {
+                    Console.WriteLine("Please enter a number");
+                } else
+                {
+                    Console.WriteLine("that is not a number, please enter a number ");
+                }
+
+                Console.ReadLine();
 
 
-                case 1:
-
-                    break;
 
 
-                case 2:
 
-                    break;
-
-                case 3:
-
-                    break;
-
-                default:
-                    break;
             }
-}
-
         }
     }
 }
