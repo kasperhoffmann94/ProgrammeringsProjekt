792c0da4fb85091f88185e34c8b7a3b4997db2f9
Kristian Kastrup Pedersen <kris9139@edu.eal.dk>
2018-09-27 10:10:58 +0200

    Bindings DoThis, DoThat, DoSomething

--- SmartMenuLibrary/Bindings.cs
+++ SmartMenuLibrary/Bindings.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FunctionLibrary;
 
 namespace SmartMenuLibrary
 {
@@ -12,7 +13,24 @@ namespace SmartMenuLibrary
 
         public string Call(string id)
         {
+            if (id == "polyline")
+            {
+                Console.WriteLine(Functions.DoThis());
+            }
 
+            else if (id == "rectanglearea")
+            {
+                Console.WriteLine(Functions.DoThat());
+            }
+
+            else if (id == "listofnumbers")
+            {
+                Console.WriteLine("please write something");
+                string temp = "";
+                temp = Console.ReadLine();
+
+                Console.WriteLine(Functions.DoSomething(temp));
+            }
 
         }
     }
